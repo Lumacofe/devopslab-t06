3073a8143cc3afb3a177c145e9d4121a2ce5abaf
Lumacofe <42089105+Lumacofe@users.noreply.github.com>
2022-08-27 15:02:43 -0300

    finallll

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - To no Docker"
+    return "Hello World - To no Docker - VOCE E 10 GABY"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
